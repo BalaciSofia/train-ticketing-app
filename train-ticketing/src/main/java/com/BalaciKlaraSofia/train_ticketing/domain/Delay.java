b124157a75e065bbb4f6c2f5ca687f14d6f08d79
BalaciSofia <klara.balaci@stud.ubbcluj.ro>
2026-05-09 15:14:27 +0300

    added stop column to delay class

--- train-ticketing/src/main/java/com/BalaciKlaraSofia/train_ticketing/domain/Delay.java
+++ train-ticketing/src/main/java/com/BalaciKlaraSofia/train_ticketing/domain/Delay.java
@@ -14,21 +14,28 @@ public class Delay {
     @JoinColumn(name = "schedule_id", nullable = false)
     private Schedule schedule;
 
+    @ManyToOne
+    @JoinColumn(name = "from_schedule_stop_id", nullable = false)
+    private ScheduleStop fromScheduleStop;
+
     @Column(nullable = false)
     private Integer delayMinutes;
 
     protected Delay() {}
 
-    public Delay(Schedule schedule, Integer delayMinutes) {
+    public Delay(Schedule schedule, ScheduleStop fromScheduleStop, Integer delayMinutes) {
         this.schedule = schedule;
+        this.fromScheduleStop = fromScheduleStop;
         this.delayMinutes = delayMinutes;
     }
 
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
     public Schedule getSchedule() { return schedule; }
+    public ScheduleStop getFromScheduleStop() { return fromScheduleStop; }
     public Integer getDelayMinutes() { return delayMinutes; }
 
     public void setSchedule(Schedule schedule) { this.schedule = schedule; }
+    public void setFromScheduleStop(ScheduleStop fromScheduleStop) { this.fromScheduleStop = fromScheduleStop; }
     public void setDelayMinutes(Integer delayMinutes) { this.delayMinutes = delayMinutes; }
 }
